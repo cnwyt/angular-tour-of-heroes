64d57e65fef3599a796ea961262f89c92c0b3483
WYT <wangtom365@qq.com>
2018-09-05 14:56:57 +0800

    Create a Hero class,Show the hero object

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+// Create a Hero class
+export class Hero {
+    id: number;
+    name: string;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// import the Hero class
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,7 +15,10 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
-  hero = 'Windstorm';
+  hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+  };
 
 
 }
